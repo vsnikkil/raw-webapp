0fad51d83bb7641c9e4737b315f4c32ea6d4569e
Vesa Nikkilä <nikkila.vesa@gmail.com>
2016-09-16 19:28:44 +0300

    added NODE_ENV=development|production support for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,29 @@
+const webpack = require('webpack')
+
 module.exports = {
+  devtool: process.env.NODE_ENV === 'production' ? 'cheap-module-source-map' : 'eval',
   module: {
-    loaders: [
-      {
-        test: /\.js?$/,
-        exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          "presets": [
-            "es2015",
-            "react",
-            "stage-0"
-          ]
-        }
+  loaders: [
+    {
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        "presets": [
+          "es2015",
+          "react",
+          "stage-0"
+        ]
       }
-    ]
-  }
+    }
+  ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+      }
+    })
+  ]
 }
 
