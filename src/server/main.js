3f2e2c39c8672f96dea136c23e6e2876f4c73468
Vesa Nikkilä <nikkila.vesa@gmail.com>
2016-09-16 17:19:12 +0300

    included etag and conditional

--- src/server/main.js
+++ src/server/main.js
@@ -3,12 +3,17 @@ import 'babel-polyfill'
 import Koa from 'koa'
 import Router from 'koa-router'
 import send from 'koa-send'
+import conditional from 'koa-conditional-get'
+import etag from 'koa-etag'
 import Debug from 'debug'
 
 import serverConfig from './serverConfig'
 
 const app = new Koa()
 const debug = Debug('Server')
+
+app.use(conditional())
+app.use(etag())
 
 app.use(async function (ctx, next) {
   if (!await send(ctx, ctx.path === '/' ? 'index.html' : ctx.path, { root: `${__dirname}/static` })) {
